1e400f506ff56f260ec8450bcef9c703a7e11f43
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-01 11:23:01 +1100

    Add class Program

--- let_condition_using_LINQ/Program.cs
+++ let_condition_using_LINQ/Program.cs
@@ -15,5 +15,30 @@ namespace let_condition_using_LINQ
 
     }
 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Object Initialization for Student class
+            List<Student> objStudent = new List<Student>{
+                    new Student{ Name="Tom",Regno="R001",Marks=80},
+                    new Student{ Name="Bob",Regno="R002",Marks=40},
+                    new Student{ Name="jerry",Regno="R003",Marks=25},
+                    new Student{ Name="Syed",Regno="R004",Marks=30},
+                    new Student{ Name="Mob",Regno="R005",Marks=70},
+                };
 
+            var objresult = from stu in objStudent
+                            let totalMarks = objStudent.Sum(mark => mark.Marks)
+                            let avgMarks = totalMarks / 5
+                            where avgMarks > stu.Marks
+                            select stu;
+            foreach (var stu in objresult)
+            {
+                Console.WriteLine("Student: {0} {1}", stu.Name, stu.Regno);
+
+            }
+            Console.ReadLine();
+        }
+    }
 }
